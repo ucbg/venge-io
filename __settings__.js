dd5f1c963b36c6d6c8786fe7caf424b41c0b4012
ucbg <hamidogs22@gmail.com>
2024-11-01 10:47:03 +0100

    Initial commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1643571.json?v=" + VERSION;
+CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': ['webgl2', 'webgl1'],
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [];
+CONFIG_FILENAME = "config.json?v=" + VERSION;
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : '//files/assets/31040882/1/ammo.wasm.js', 'wasmUrl' : '//files/assets/31040883/1/ammo.wasm.wasm?v=1.0', 'fallbackUrl' : '//files/assets/31040881/1/ammo.js?v=1.0', 'preload' : true}
+];
